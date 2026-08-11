0db6341d8cbc984c6ba1d313703380daf2fab9b4
rishabh-kumar-3935 <rishabhkumar13935@gmail.com>
2026-08-11 23:43:22 +0530

    healthControllerUpdate

--- backend/src/controllers/healthcheck.controller.js
+++ backend/src/controllers/healthcheck.controller.js
@@ -1,6 +1,6 @@
-import { ApiError } from '../utils/apiError.js'
+import { ApiError } from '../utils/ApiError.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
-import { ApiResponse } from '../utils/apiResponse.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
 
 const healthcheck = asyncHandler(async(req ,res)=>{
     return res
